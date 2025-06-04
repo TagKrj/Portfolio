e1971eeb35e68cebbdd719c2023528506d648722
TagKrj <trangbui19032004@gmail.com>
2025-06-04 22:45:58 +0700

    commit2

--- src/sections/Testimonial.jsx
+++ src/sections/Testimonial.jsx
@@ -17,8 +17,8 @@ const ReviewCard = ({
             className={twMerge(
                 "relative h-full w-64 cursor-pointer overflow-hidden rounded-xl border p-4 border-gray-50/[.1] bg-gradient-to-r bg-indigo to-storm hover:bg-royal hover-animation",
 
-            )
-                >
+            )}
+        >
             <div className="flex flex-row items-center gap-2">
                 <img className="rounded-full bg-white/10" width="32" height="32" alt="" src={img} />
                 <div className="flex flex-col">
@@ -29,7 +29,7 @@ const ReviewCard = ({
                 </div>
             </div>
             <blockquote className="mt-2 text-sm">{body}</blockquote>
-        </figure >
+        </figure>
     );
 };
 
